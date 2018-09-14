a401e5ad807b6150d6ad137958189abbb307e1d2
Jared Butcher <jared.butcher1219@gmail.com>
2018-09-14 12:28:15 -0400

    serialConn and wsServer interfaces created

--- robot/serialConn.py
+++ robot/serialConn.py
@@ -3,3 +3,50 @@ Connections will be global
 '''
 import serial
 import serial.tools.list_ports
+import threading
+
+
+def getSerialConn(id):
+    '''Returns a serial connection to a spicific device denoted by id
+
+        Args:
+            id (string): identifer of serial device
+
+        Returns (serialConn): connection requested, returns None if none is found
+    '''
+    pass
+
+class serialConn:
+    '''Handles one serial connection
+    '''
+    def send(self, message):
+        '''Sends a message to device
+
+            Args:
+                message (string): messge to send
+        '''
+        pass
+    
+    def addRecCallback(self, callback):
+        '''Adds a function to call upown receiving a message
+
+            Args:
+                callback (function(id, message)): function to call
+                    Args:
+                        id (string): assigned identifer of the serial device
+                        message (string): received messge
+        '''
+        pass
+
+    def rmRecCallback(self, callback):
+        '''Try to remove callback from functions to call upown receiveing a message
+
+            Args:
+                callback (function(id, message)): function to remove
+        '''
+        pass
+
+    def restablish(self):
+        '''Recreate connection and restart arduino device in the process
+        '''
+        pass
--- robot/wsServer.py
+++ robot/wsServer.py
@@ -9,3 +9,41 @@ import asyncio
 import threading
 import json
 import websockets
+
+def command(funct):
+    '''Decorator marks function that can be called on client and runs on server
+
+        Decorated Function Args:
+            client (websocketClient, optional) contains client message originated from
+    '''
+    def decorator(*args, **kwargs):
+        funct(client=None, *args, **kwargs)
+    return decorator
+
+def clientRPC(funct):
+    '''Decorator marks function that can be called on server and runs one or more clients
+
+        Decorated Function Args:
+            keyword args: clients to send to
+                accepted keywords: target[websocketClient], blob[bytes]
+        If no targets are given it sends to all
+        If blob is given then the command will be sent, followed by blob
+    '''
+    def decorator(*args, **kwargs):
+        funct(*args, **kwargs)
+    return decorator
+
+class websocketClient:
+    '''Handles a websocket connction to one client
+    ''' 
+
+    def _send(self, message, isBlob=False):
+        '''Sends message to client
+
+            Args:
+                message (string or dict or blob): message to be sent
+        '''
+        pass
+
+    def _destory(self):
+        pass
